1905850b295d85806e20c9703f3a3c373de3ad35
EDUARDO SILVA <educwb@gmail.com>
2023-01-01 09:42:40 -0300

    organizando os TODOs

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,11 @@ import { useState } from 'react';
 // * Não é permitido usar refs
 //
 // Tarefas:
-// todo - O botão de login deve disparar a função login(), importada no topo deste arquivo, e passar os dados necessários.
-// todo - Desabilite o botão de Login caso o e-mail esteja em branco OU a senha for menor que 6 dígitos.
-// todo - Desabilite o botão de Login enquanto você está executando o login.
-// todo - Mostre uma mensagem de erro de login() caso o Login falhe. A mensagem deve ser limpa a cada nova tentativa de Login.
-// todo - Mostre um alerta caso o login seja efetuado com sucesso (javascript alert). Investigue a função login() para entender como ter sucesso na requisição.
+// todo 01 - Desabilite o botão de Login caso o e-mail esteja em branco OU a senha for menor que 6 dígitos.
+// todo 02 - O botão de login deve disparar a função login(), importada no topo deste arquivo, e passar os dados necessários.
+// todo 03 - Desabilite o botão de Login enquanto você está executando o login.
+// todo 04 - Mostre uma mensagem de erro de login() caso o Login falhe. A mensagem deve ser limpa a cada nova tentativa de Login.
+// todo 05 - Mostre um alerta caso o login seja efetuado com sucesso (javascript alert). Investigue a função login() para entender como ter sucesso na requisição.
 
 export default function LoginForm() {
   return (
