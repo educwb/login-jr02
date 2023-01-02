6998c11bb6d9477e8c28bbfe82f26f69f704a9b0
EDUARDO SILVA <educwb@gmail.com>
2023-01-02 12:57:36 -0300

    to-do 01 done

--- src/App.jsx
+++ src/App.jsx
@@ -15,23 +15,37 @@ import { useState } from 'react';
 // todo 05 - Mostre um alerta caso o login seja efetuado com sucesso (javascript alert). Investigue a função login() para entender como ter sucesso na requisição.
 
 export default function LoginForm() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleEmail = (event) => {
+    const { value } = event.target
+    setEmail(value)
+  }
+
+  const handlePassword = (event) => {
+    const { value } = event.target
+    setPassword(value)
+  }
+
   return (
     <div className='wrapper'>
       <div className='login-form'>
-        <h1>Login Form 🐞</h1>
+        <h1>Login Practice 01 🐞</h1>
         {/* Coloque a mensagem de erro de login na div abaixo. Mostre a div somente se houver uma mensagem de erro. */}
         <div className='errorMessage'></div>
         <div className='row'>
           <label htmlFor={'email'}>Email</label>
-          <input id={'email'} type={'email'} autoComplete='off' />
+          <input id={'email'} type={'email'} autoComplete='off' value={email} onChange={handleEmail} />
         </div>
         <div className='row'>
           <label htmlFor={'password'}>Password</label>
-          <input id={'password'} type={'password'} />
+          <input id={'password'} type={'password'} value={password} onChange={handlePassword} />
         </div>
 
         <div className='button'>
-          <button>Login</button>
+          <button disabled={email === '' || password.length < 6}>Login</button>
         </div>
       </div>
     </div>
